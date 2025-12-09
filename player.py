a1038e4592bed3531f282f276e8f7be26eae0c8b
Driss SAAFI <drisssaafi3@gmail.com>
2025-12-09 09:07:40 +0100

    initialisation taille personnage

--- player.py
+++ player.py
@@ -3,4 +3,5 @@ import pygame
 class Player:
     def __init__(self):
         self.image = pygame.image.load("perso_face_fin.png")
+        self.image = pygame.transform.scale(self.image, (64, 100))
         self.body = self.image.get_rect()
