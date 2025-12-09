88a42e2869f52a1ade9f08a0eafd4fb70e6db9b4
abde079 <abde2007b@gmail.com>
2025-12-09 09:55:24 +0100

    image(g,m,d)
    il faut faire pour aller a droite

--- player.py
+++ player.py
@@ -2,6 +2,12 @@ import pygame
 
 class Player:
     def __init__(self):
-        self.image = pygame.image.load("perso_face_fin.png")
-        self.image = pygame.transform.scale(self.image, (64, 100))
+        self.image=pygame.image.load("perso_face_fin.png")
+        self.image=pygame.transform.scale(self.image,(64,100))
+        self.image_face = pygame.image.load("perso_face_fin.png")
+        self.image_face = pygame.transform.scale(self.image_face, (64, 100))
+        self.image_gauche=pygame.image.load("perso_profile_gauche_fin.png")
+        self.image_gauche = pygame.transform.scale(self.image_gauche, (64, 100))
+        self.image_dos=pygame.image.load("perso_dos_fin.png")
+        self.image_dos = pygame.transform.scale(self.image_dos, (64, 100))
         self.body = self.image.get_rect()
