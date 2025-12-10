ed8c31c60f0f16a2eca6cabc0ac7fbd5b15a6614
Driss SAAFI <drisssaafi3@gmail.com>
2025-12-10 15:47:17 +0100

    map et deplacement fini
    fait avec Hippolyte

--- player.py
+++ player.py
@@ -3,17 +3,17 @@ import pygame
 class Player:
     def __init__(self):
         self.image=pygame.image.load("perso_face_fin.png")
-        self.image=pygame.transform.scale(self.image,(64,100))
+        self.image=pygame.transform.scale(self.image,(40,80))
         
         self.image_face = pygame.image.load("perso_face_fin.png")
-        self.image_face = pygame.transform.scale(self.image_face, (64, 100))
+        self.image_face = pygame.transform.scale(self.image_face, (40, 80))
         
         self.image_gauche=pygame.image.load("perso_profile_gauche_fin.png")
-        self.image_gauche = pygame.transform.scale(self.image_gauche, (64, 100))
+        self.image_gauche = pygame.transform.scale(self.image_gauche, (40, 80))
         
         self.image_dos=pygame.image.load("perso_dos_fin.png")
-        self.image_dos = pygame.transform.scale(self.image_dos, (64, 100))
+        self.image_dos = pygame.transform.scale(self.image_dos, (40, 80))
         
         self.image_droite = pygame.transform.flip(self.image_gauche, True, False)
         
-        self.body = self.image.get_rect()
+        self.body = pygame.Rect((920, 960),(40,80))
--- screen.py
+++ screen.py
@@ -1,6 +1,7 @@
 import pygame
 from grid import set_grid
 from player import Player
+from map import gmap
 
 class Screen:
     def __init__(self):
@@ -8,29 +9,34 @@ class Screen:
         pygame.display.toggle_fullscreen()
         self.run = True
         self.player = Player()
+        self.background=pygame.image.load("resized_image.png")
 
     def start(self):
         
         while self.run:
                 
-            self.screen.fill((0,0,0))
-            self.screen.blit(self.player.image, self.player.body)
+            self.screen.blit(self.background, (0, 0))
+            self.screen.blit(self.player.image,dest=self.player.body)
+            
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT and self.player.body.left+40<1920:
+                    my = self.player.body.left//40
+                    mx = self.player.body.top//40
+                    if event.key == pygame.K_RIGHT and gmap[mx][my+1] == 0:
                         self.player.body.left+=40
                         self.player.image=self.player.image_droite
-                    if event.key == pygame.K_LEFT and self.player.body.left-40>=0: 
+                    if event.key == pygame.K_LEFT and gmap[mx][my-1] == 0: 
                         self.player.body.left-=40
                         self.player.image=self.player.image_gauche
-                    if event.key == pygame.K_UP and self.player.body.top-40>=0:
+                    if event.key == pygame.K_UP and gmap[mx-1][my] == 0:
                         self.player.body.top-=40
                         self.player.image=self.player.image_dos
-                    if event.key == pygame.K_DOWN and self.player.body.top+40<1080:
+                    if event.key == pygame.K_DOWN and gmap[mx+1][my] == 0:
                         self.player.body.top+=40
                         self.player.image=self.player.image_face
                 if event.type == pygame.QUIT:
                     self.run = False
+                
                 
                     
                 pygame.display.update()
