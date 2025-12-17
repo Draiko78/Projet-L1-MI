136391697f21ce73af7b63c68ffd20de9cb875e1
yanislamri <yanis.lamri08@outlook.fr>
2025-12-17 15:38:21 +0100

    Async test pour web

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import asyncio
 import pygame
 from screen import Screen
 
 pygame.init()
+async def main():
+  game = Screen()
+  game.start()
 
-game = Screen()
-game.start()
+  pygame.quit()
 
-pygame.quit()
+asyncio.run(main())
