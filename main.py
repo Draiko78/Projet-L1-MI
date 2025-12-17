47721630a10d31d6f3434dd1ba0877b765abefc2
yanislamri <yanis.lamri08@outlook.fr>
2025-12-17 19:39:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import asyncio
 import pygame
 from screen import Screen
 
 pygame.init()
-async def main():
-  game = Screen()
-  game.start()
-  await asyncio.sleep(0)
-  pygame.quit()
 
-asyncio.run(main())
+game = Screen()
+game.start()
+
+pygame.quit()
